596a730beb7acd09230770ab5242f54997a52481
cpasyanos <pasyanos.c@husky.neu.edu>
2017-07-20 16:32:04 -0400

    Fixed outline toggling

---
+++ Assets/Scripts/TargetController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using cakeslice;
+
+public class TargetController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Outline>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
