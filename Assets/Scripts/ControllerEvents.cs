f2680fdbae8a854f0898d77c1015e76a845ba589
cpasyanos <pasyanos.c@husky.neu.edu>
2017-08-14 16:43:02 -0400

    worked on events

---
+++ Assets/Scripts/ControllerEvents.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerEvents : MonoBehaviour {
+
+    // define delegates and events
+
+    // trigger the events
+
+    // subscribing gameobjects
+
+    public delegate void ControllerHandler(GameObject obj);
+
+    public static event ControllerHandler onTriggerDown;
+
+}
