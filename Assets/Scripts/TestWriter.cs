64ca661e05a8c1e1036d28bc16ef6aef2540da94
cpasyanos <pasyanos.c@husky.neu.edu>
2017-09-21 14:07:52 -0400

    Tested ability to write data to a file

---
+++ Assets/Scripts/TestWriter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class TestWriter : MonoBehaviour {
+
+    private List<string> data;
+
+    private void WriteToFile()
+    {
+        using (StreamWriter file = new StreamWriter(@"C:\Users\Rehabilitation Games\Desktop\CharlesHoodGrantTask\Assets\Data\test.txt"))
+        {
+            file.Write("Writing to file 2");
+            file.Close();
+        }
+    }
+
+	void OnDisable()
+    {
+        WriteToFile();
+    }
+    
+    // Use this for initialization
+	void Start () {
+        using (StreamWriter file = new StreamWriter(@"C:\Users\Rehabilitation Games\Desktop\CharlesHoodGrantTask\Assets\Data\test.txt"))
+        file.Write("Writing to file");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
