7e849fdb4f73fc024f3af37497fdb92360abd10f
Eric Wilson <SPHEREGEN\eric.wilson@spheregen.com>
2019-02-22 11:19:20 -0500

    initial commit of the new steam vr plugin and minor script changes.

--- Assets/Scripts/Controller and Manus Scripts/ViveControllerHandler.cs
+++ Assets/Scripts/Controller and Manus Scripts/ViveControllerHandler.cs
@@ -1,37 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class ViveControllerHandler : MonoBehaviour {
 
     public Canvas canvas;
-     
-    // Controller and access to controller input
-    private SteamVR_TrackedObject ctrlr;
-    private SteamVR_Controller.Device Controller
-    {
-        get { return SteamVR_Controller.Input((int)ctrlr.index); }
-    }
+	// EW - The purpose of this script is unknown. it is not referenced and is outdated, so it will be commented out.
 
-    // grab the tracked object component from the controllers - 
-    // auto-attached in SteamVR prefab
-    void Awake()
-    {
-        ctrlr = GetComponent<SteamVR_TrackedObject>();
-    }
+	// Controller and access to controller input
+	// private SteamVR_TrackedObject ctrlr;
+	//private SteamVR_Controller.Device Controller
+	//{
+	//	get { return SteamVR_Controller.Input((int)ctrlr.index); }
+	//}
+
+	// grab the tracked object component from the controllers - 
+	// auto-attached in SteamVR prefab
+	//void Awake()
+ //   {
+ //       ctrlr = GetComponent<SteamVR_TrackedObject>();
+ //   }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Controller.GetHairTriggerDown())
-        {
-            canvas.GetComponent<Calibrate>().CaptureArmLengthShoulderHeight();
-        }
+    //void Update()
+    //{
+    //    if (Controller.GetHairTriggerDown())
+    //    {
+    //        canvas.GetComponent<Calibrate>().CaptureArmLengthShoulderHeight();
+    //    }
 
-        /* debug purposes
-        if (Controller.GetHairTriggerDown())
-        {
-            Debug.Log(gameObject.name + " Trigger Press");
-        }*/
-    }
+    //    /* debug purposes
+    //    if (Controller.GetHairTriggerDown())
+    //    {
+    //        Debug.Log(gameObject.name + " Trigger Press");
+    //    }*/
+    //}
 }
