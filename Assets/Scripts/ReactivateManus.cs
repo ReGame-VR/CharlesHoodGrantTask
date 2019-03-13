2ef153f43f5b42014e0c2c4f9edd42a0a1f09eff
mtsandmeyer <32112441+mtsandmeyer@users.noreply.github.com>
2019-03-13 18:01:14 -0400

    bug fixed and tracking fixes
    set all tracking objects to use the pose behavior and the input bindings required
    fixed some things that broke with the sdk upgrade, references that had been lost.
    calibrated the right manus glove.
    other minor changes

---
+++ Assets/Scripts/ReactivateManus.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// some strange issue causes manus gameobject renderers to deactivate without reason. adding this script to the arm will cause them to reactivate immediately on the second frame
+/// </summary>
+public class ReactivateManus : MonoBehaviour {
+
+	void Start () {
+        StartCoroutine(EnableRenderers());
+	}
+
+    IEnumerator EnableRenderers()
+    {
+        yield return new WaitForEndOfFrame();
+        foreach (var skinnedRenderer in GetComponentsInChildren<SkinnedMeshRenderer>())
+        {
+            skinnedRenderer.enabled = true;
+        }
+    }
+
+}
